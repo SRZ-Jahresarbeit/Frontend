1f9e757dd85d086991ed04ff817841543810c356
CaspanB <berg.4.fhmdd@gmail.com>
2021-05-13 23:36:11 +0200

    finished function to Create Chart from API Data

--- src/index.js
+++ src/index.js
@@ -180,7 +180,7 @@ async function processAPIGeDa(){
         }
     }
 
-    /*
+    
     var options = {
         chart: {
             type: 'line'
@@ -198,24 +198,6 @@ async function processAPIGeDa(){
 
     var chart = new ApexCharts(document.querySelector("#chart"), options);
     chart.render();
-    */
-
-    var options = {
-        chart: {
-          type: 'line'
-        },
-        series: [{
-          name: 'sales',
-          data: [30,40,35,50,49,60,70,91,125]
-        }],
-        xaxis: {
-          categories: [1991,1992,1993,1994,1995,1996,1997, 1998,1999]
-        }
-      }
-      
-      var chart = new ApexCharts(document.querySelector(".chart"), options);
-      
-      chart.render();
 
     //1620934260
 }
